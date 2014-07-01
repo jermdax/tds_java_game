788c150a7947e56b14ab0d470d574d31f3ba051e
jermdax <jdavidquinn@hotmail.com>
2014-07-01 21:14:04 +0100

    blank networking files didnt commit last time

---
+++ by_software/engine/networking/server/Server.java
@@ -0,0 +1,12 @@
+public class Server
+{
+	private static int connectedPlayers = 0;
+
+	public Server()
+	{
+		//set things up
+		
+		//listen for players
+			//create 2 threads for each connected player (listen and send)
+	}
+}
