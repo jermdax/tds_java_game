9ecbf34c165bffcc086e5217348ca865365c589f
jermdax <jdavidquinn@hotmail.com>
2014-07-21 16:41:16 +0100

    top down now renders actual tiles

--- by_software/engine/render/perspective/PerspectiveTopDown.java
+++ by_software/engine/render/perspective/PerspectiveTopDown.java
@@ -2,6 +2,7 @@ package by_software.engine.render.perspective;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import by_software.Game;
 import by_software.map.Map;
@@ -11,33 +12,38 @@ import by_software.mob.player.Player;
 public class PerspectiveTopDown implements Perspective
 {
   private static Map worldMap;
+  //TODO dont hardcode in the size
+  private static BufferedImage mapImg;
 
   public PerspectiveTopDown(Map worldMap)
   {
     this.worldMap = worldMap;
+    mapImg = new BufferedImage((int)(worldMap.getMapSizeX() * MapTile.getTopDownSize().getWidth()), 
+                               (int)(worldMap.getMapSizeY() * MapTile.getTopDownSize().getHeight()), 
+                               BufferedImage.TYPE_INT_ARGB);
+    constructMap( this.worldMap.getTiles() );
+  }
+
+  private void constructMap(MapTile[][] tiles)
+  {
+    Graphics g = mapImg.createGraphics();
+    int 
+      sizeX = (int)MapTile.getTopDownSize().getWidth(), 
+      sizeY = (int)MapTile.getTopDownSize().getHeight();
+    //draw the map
+    for(int x = 0; x < tiles[0].length; x++)
+      for(int y = 0; y < tiles.length; y++)
+      {
+        g.drawImage(tiles[x][y].getTopSprite(), x*sizeX, y*sizeY, Color.BLACK, null);
+      }
 
   }
 
  public void render(Graphics gr, int screenWidth, int screenHidth ,Game game, Player player)
   {
-    MapTile[][] mapTiles = worldMap.getTiles();
-    for(int x = 0; x < mapTiles.length; x++)
-    {
-      for(int y = 0; y < mapTiles[x].length; y++)
-      {
-        if( mapTiles[x][y].isWall() )
-          gr.setColor(Color.RED);
-        else
-          gr.setColor(Color.GRAY);
-        gr.fillRect(x*MapTile.getTopDownSize().width, y*MapTile.getTopDownSize().height,MapTile.getTopDownSize().width, MapTile.getTopDownSize().height);
-        
-        //drawGrid
-        gr.setColor(Color.CYAN);
-        gr.drawRect(x*MapTile.getTopDownSize().width, y*MapTile.getTopDownSize().height, MapTile.getTopDownSize().width, MapTile.getTopDownSize().height);
-      }
-      gr.setColor(Color.GREEN);
-      gr.fillOval((int)(player.getPosX()*MapTile.getTopDownSize().width)-4, (int)(player.getPosY()*MapTile.getTopDownSize().height)-4, 18, 18);
-    }
+    gr.drawImage(mapImg, 0, 0, Color.BLACK, null);
+    //draw player
+    gr.setColor(Color.GREEN);
+    gr.fillOval((int)(player.getPosX()*16-4), (int)(player.getPosY()*16-4), 8, 8);
   }
-
 }
